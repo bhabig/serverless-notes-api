c5b33545defd989d8a859b99a61aa73fd6956f7b
bhabig <rfhab2@gmail.com>
2021-02-09 22:18:09 -0500

    serverless user authentication'

--- delete.js
+++ delete.js
@@ -1,18 +1,18 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-export const main = handler(async (event, context) => {
+export const main = handler(async(event, context) => {
 
-  const params = {
-    TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be updated
-    Key: {
-      userId: "123", // The id of the author
-      noteId: event.pathParameters.id, // The id of the note from the path
-    },
-  };
+    const params = {
+        TableName: process.env.tableName,
+        // 'Key' defines the partition key and sort key of the item to be updated
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+            noteId: event.pathParameters.id, // The id of the note from the path
+        },
+    };
 
-  await dynamoDb.update(params);
+    await dynamoDb.update(params);
 
-  return { status: true };
+    return { status: true };
 });
--- list.js
+++ list.js
@@ -1,22 +1,22 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-export const main = handler(async (event, context) => {
-  const params = {
-    TableName: process.env.tableName,
-    // 'KeyConditionExpression' defines the condition for the query
-    // - 'userId = :userId': only return items with matching 'userId'
-    //   partition key
-    KeyConditionExpression: "userId = :userId",
-    // 'ExpressionAttributeValues' defines the value in the condition
-    // - ':userId': defines 'userId' to be the id of the author
-    ExpressionAttributeValues: {
-        ":userId": "123",
-    },
-  };
+export const main = handler(async(event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        // 'KeyConditionExpression' defines the condition for the query
+        // - 'userId = :userId': only return items with matching 'userId'
+        //   partition key
+        KeyConditionExpression: "userId = :userId",
+        // 'ExpressionAttributeValues' defines the value in the condition
+        // - ':userId': defines 'userId' to be the id of the author
+        ExpressionAttributeValues: {
+            ":userId": event.requestContext.identity.cognitoIdentityId, // The id of the author
+        },
+    };
 
-  const result = await dynamoDb.query(params);
+    const result = await dynamoDb.query(params);
 
-  // Return the retrieved item
-  return result.Items;
+    // Return the retrieved item
+    return result.Items;
 });
